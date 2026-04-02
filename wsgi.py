3f1a24ad10f566dc994afe62c1863fb838489130
swede07 <149293182+swede07@users.noreply.github.com>
2026-04-02 14:39:14 +0200

    Create wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+# Lägg till din projektmapp i system-path
+sys.path.insert(0, "/home/williamj/Documents/slutApplikation")
+
+# Importera 'app' från din huvudfil (byt 'main' mot namnet på din python-fil, t.ex. 'app')
+from app import app as application
